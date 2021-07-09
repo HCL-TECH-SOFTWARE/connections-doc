35771e98eff323b109601f1353af58a7238e74e6
dasayer <53905149+davidsayer57@users.noreply.github.com>
2021-07-09 08:01:11 -0400

    url correction

--- docs/oxygen-webhelp/app/config.js
+++ docs/oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210707143049';
+                args = '20210709075747';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
