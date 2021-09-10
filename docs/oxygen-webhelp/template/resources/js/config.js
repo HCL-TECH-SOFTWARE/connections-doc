68a74bde3217612526e976c202981f54c04693f9
dasayer <david.sayer@hcl.com>
2021-09-10 10:20:26 -0400

    Update config.js

--- docs/oxygen-webhelp/template/resources/js/config.js
+++ docs/oxygen-webhelp/template/resources/js/config.js
@@ -22,9 +22,8 @@ export const ENABLE_LANG_EN = false; // This should be the same configuration fo
 export const LEARNING_ACADEMY_LINK = true // Set to true to enable Learning Academy link in the header.
 
 //Change Product Name to your actual product name. ex: Digital Experience
-//export const VERSION_LIST = {
-//    "0" : "Connections MT",
-//}
+export const VERSION_LIST = {
+}
 //Change the list base on your product languages availability.
-//export const PROD_LANG_LIST = ["en"]
+export const PROD_LANG_LIST = ["en"]
 // export const PROD_LANG_LIST = ["en", "ar", "bg", "ca", "zh_CN", "zh_TW", "hr", "cs", "da", "nl", "fi", "fr", "de", "iw", "it", "hu", "ja", "kk", "ko", "no", "pl", "pt_BR", "pt", "ro", "ru", "sk", "sl", "es", "sv", "th", "tr"]
